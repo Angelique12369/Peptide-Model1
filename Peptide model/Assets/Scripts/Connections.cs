67fef35b091605bd5817e399280c0bff9eaab1fa
Angelique12369 <66284259+Angelique12369@users.noreply.github.com>
2020-09-23 15:05:22 -0400

    Initial Commit

---
+++ Peptide model/Assets/Scripts/Connections.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connections : MonoBehaviour
+{
+    public Transform m_peptideBond;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.transform == m_peptideBond)
+        {
+            gameObject.AddComponent<FixedJoint>().connectedBody = m_peptideBond.GetComponent<Rigidbody>();
+            this.enabled = false;
+        }
+    }
+}
---
+++ Peptide model/Assets/Scripts/DistanceMaintenance.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceMaintenance : MonoBehaviour
+{
+    public Transform m_previousSphere;
+    public Transform m_nextSphere;
+    public float m_bondLength;
+    public float m_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_previousSphere)
+        {
+            if (Vector3.Distance(m_previousSphere.position,transform.position)>m_bondLength)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, m_previousSphere.position,m_speed);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, m_previousSphere.position, -m_speed);
+            }
+        }
+        if (m_nextSphere)
+        {
+            if (Vector3.Distance(m_nextSphere.position, transform.position) > m_bondLength)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, m_nextSphere.position, m_speed);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, m_nextSphere.position, -m_speed);
+            }
+        }
+    }
+}
